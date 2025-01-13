06269267a7dd59700667f34c05f227bef3ef4c48
Avishkar Gawali <119069300+avishkar448@users.noreply.github.com>
2025-01-13 12:50:56 +0530

    server

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-import express from "express"
-import cors from "cors"
-import 'dotenv/config'
-import cookieParser from 'cookie-parser'
-import connectDB from "./config/mongodb.js"
-import authRouter from './routes/authRoutes.js'
-import userRouter from "./routes/userRoutes.js"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
-const app=express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins = ["https://iridescent-otter-c01291.netlify.app"];
+//const allowedOrigins=['http://localhost:5173']
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({origin: allowedOrigins,credentials: true}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({ origin: allowedOrigins, methods: ["POST", "GET"], credentials: true })
+);
 
 //api
-app.get('/',(req,res)=> res.send("API Working Fine"))
-app.use('/api/auth',authRouter)
-app.use('/api/user',userRouter)
+app.get("/", (req, res) => res.send("API Working Fine"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.listen(port,()=> console.log(`Server running on ${port}`));
+app.listen(port, () => console.log(`Server running on ${port}`));
