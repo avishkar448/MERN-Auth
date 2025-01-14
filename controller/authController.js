c4911c69f6bfb9d73f3cdc4a6d948eb75bd146e7
Avishkar Gawali <119069300+avishkar448@users.noreply.github.com>
2025-01-14 11:42:45 +0530

    update

--- controller/authController.js
+++ controller/authController.js
@@ -98,10 +98,10 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true, // Ensures cookies are sent over HTTPS
-      sameSite: "None", // Allows cross-origin requests
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: "None", // Allows cross-origin requests
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      path:'/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
